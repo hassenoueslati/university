a8c6c1386aed09f0fee486d505f31ebd2614832b
0Stain <louay.jeddou@esprit.tn>
2022-11-03 19:25:32 +0100

    Clubs OrderedBy Nom + Nb Membres

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BatimentComponent} from "./batiment/batiment.component";
-
+import {ClubComponent} from "./club/club.component";
 const routes: Routes = [
   { path: 'batiment',component: BatimentComponent},
+  { path: 'club',component: ClubComponent},
 ];
 
 @NgModule({
--- Server/src/main/java/com/university/RestApi.java
+++ Server/src/main/java/com/university/RestApi.java
@@ -36,8 +36,77 @@ public class RestApi {
         }
     }
     
+    @GetMapping("/club")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public String afficherClub() {
+        String NS = "";
+        // lire le model a partir d'une ontologie
+        if (model != null) {
+            // lire le Namespace de l�ontologie
+            NS = model.getNsPrefixURI("");
+
+            // apply our rules on the owlInferencedModel
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            // query on the model after inference
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_Club.txt");
+            System.out.println(res);
+            return res.toString();
+
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
+    
+    @GetMapping("/clubtri")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public String afficherclubOrdered() {
+        String NS = "";
+        // lire le model a partir d'une ontologie
+        if (model != null) {
+            // lire le Namespace de l�ontologie
+            NS = model.getNsPrefixURI("");
+
+            // apply our rules on the owlInferencedModel
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            // query on the model after inference
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_ClubTri.txt");
+            System.out.println(res);
+            return res.toString();
+
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
+    
+    @GetMapping("/clubtrinom")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public String afficherclubOrderedNomComplet() {
+        String NS = "";
+        // lire le model a partir d'une ontologie
+        if (model != null) {
+            // lire le Namespace de l�ontologie
+            NS = model.getNsPrefixURI("");
+
+            // apply our rules on the owlInferencedModel
+            Model inferedModel = JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+
+            // query on the model after inference
+            OutputStream res =  JenaEngine.executeQueryFile(inferedModel, "data/query_ClubTriNom.txt");
+            System.out.println(res);
+            return res.toString();
+
+
+        } else {
+            return ("Error when reading model from ontology");
+        }
+    }
+    
     @GetMapping("/classe")
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:4200")
     public String afficherClasse() {
         String NS = "";
         // lire le model a partir d'une ontologie
